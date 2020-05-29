2e597a93e5b8f65ca3e0cd8422ba7663d0081d2c
kulloveth <kulloveth@gmail.com>
2020-05-29 20:30:26 +0100

    Initial commit

---
+++ src/Card.kt
@@ -0,0 +1,58 @@
+data class Card(val pip: String, val suit: Char)
+
+val createDeck = fun(): MutableSet<Card> {
+    val suits = listOf('\u2663', '\u2660', '\u2666', '\u2665')
+    val pips = listOf(
+        "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+        "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+        "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+        "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10"
+    )
+    val deck = mutableSetOf<Card>()
+    for (p in pips.indices) {
+        for (s in suits.indices) {
+            deck.add(Card(pips[p], suits[s]))
+        }
+    }
+    return deck
+}
+
+val dealHand = fun(deck: MutableSet<Card>, noOfCards: Int): MutableList<Card> {
+    val hand = mutableListOf<Card>()
+    for (i in 1..noOfCards) {
+        val card = deck.random()
+        hand.add(card)
+        deck.remove(card)
+    }
+    return hand
+}
+val evaluateHand = fun(hand: MutableList<Card>): Int {
+    var total = 0
+    for (card in hand) {
+        val pipValue = when (val pip = card.pip) {
+            "J",
+            "Q",
+            "K" -> 10
+            "A" -> 11
+            else -> pip.toInt()
+        }
+        total += pipValue
+    }
+    return total
+}
+val printResult = fun(hand: MutableList<Card>, total: Int) {
+    var message = ""
+    val cards: ArrayList<String> = arrayListOf()
+    for (card in hand) {
+        cards.add("Your Hand was ${card.pip} ${card.suit}")
+        message = when (total) {
+            21 -> "For a total of $total \n You are a winner"
+            22 -> "For a total of $total \n You Lose!"
+            else -> "For a total of $total"
+        }
+    }
+    cards.forEach {
+        println(it)
+    }
+    println(message)
+}
---
+++ src/Test.kt
@@ -0,0 +1,12 @@
+class Test {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val deal = dealHand(createDeck(), 2)
+            val evaluate = evaluateHand(deal)
+            printResult(deal, evaluate)
+        }
+    }
+}
