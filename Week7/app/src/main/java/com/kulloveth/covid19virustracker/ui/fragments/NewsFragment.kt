e5cfc870b1a5b68f4d5c9eaf8b1d0efce51f254b
kulloveth <kulloveth@gmail.com>
2020-07-12 19:23:35 +0100

    setup nav graph

---
+++ Week7/app/src/main/java/com/kulloveth/covid19virustracker/ui/fragments/NewsFragment.kt
@@ -0,0 +1,24 @@
+package com.kulloveth.covid19virustracker.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.kulloveth.covid19virustracker.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class NewsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+}
