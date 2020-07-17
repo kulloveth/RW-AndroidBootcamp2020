f951df39aa7cd704a53377beeaa4071e7623a291
kulloveth <kulloveth@gmail.com>
2020-07-17 10:54:00 +0100

    check for null value and lock screen orientation

--- Week7/app/src/main/java/com/kulloveth/covid19virustracker/model/StatusResponse.kt
+++ Week7/app/src/main/java/com/kulloveth/covid19virustracker/model/StatusResponse.kt
@@ -23,16 +23,16 @@ data class Data(
 data class CountryStatus(
     @PrimaryKey
     val country: String,
-    val country_abbreviation: String,
-    val total_cases: String,
-    val new_cases: String,
-    val total_deaths: String,
-    val new_deaths: String,
-    val total_recovered: String,
-    val active_cases: String,
-    val serious_critical: String,
-    val cases_per_mill_pop: String,
-    val flag: String
+    val country_abbreviation: String? = "",
+    val total_cases: String? = "",
+    val new_cases: String? = "",
+    val total_deaths: String? = "",
+    val new_deaths: String? = "",
+    val total_recovered: String? = "",
+    val active_cases: String? = "",
+    val serious_critical: String? = "",
+    val cases_per_mill_pop: String? = "",
+    val flag: String? = ""
 ) : Parcelable
 
 data class PaginationMeta(
